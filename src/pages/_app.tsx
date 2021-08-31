7ccf9a774ee814c2c1717f9ea717b3bdef83869b
iTomasi <duclosiw@gmail.com>
2021-08-31 11:27:32 -0400

    adding one route

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import "../scss/App.scss";
+import Link from "next/link";
 
 const App = ({ Component, pageProps }) => {
     return (
         <>
+            <header>
+                <Link href="/">
+                    <a>Home</a>
+                </Link>
+                <Link href="/testing">
+                    <a>Test</a>
+                </Link>
+            </header>
             <Component {...pageProps} />
         </>
     );
---
+++ src/pages/testing.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Testing = () => {
+    return (
+        <>
+            <h1>TESTING</h1>
+        </>
+    );
+};
+
+export default Testing;
